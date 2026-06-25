5b954d4626cc2acaa1c997b3c8c0e9161557d972
Gayathri N <gayatrigayathri703@gmail.com>
2026-06-25 18:16:46 +0530

    try port 587 for email

--- utils/emailService.js
+++ utils/emailService.js
@@ -2,15 +2,16 @@ const nodemailer = require('nodemailer');
 
 const transporter = nodemailer.createTransport({
   host: 'smtp.gmail.com',
-  port: 465,
-  secure: true,
+  port: 587,
+  secure: false,
+  requireTLS: true,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS
   },
-  connectionTimeout: 10000,
-  greetingTimeout: 10000,
-  socketTimeout: 10000
+  connectionTimeout: 15000,
+  greetingTimeout: 15000,
+  socketTimeout: 15000
 });
 
 transporter.verify((error, success) => {
